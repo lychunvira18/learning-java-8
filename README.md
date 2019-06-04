9c64ef3c64a5e38790ca08e9f93a9792d847a77d
lychunvira18 <lychunvira18@kit.edu.kh>
2019-06-04 09:50:14 +0700

    Java Lesson 9 by Derek Banas

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
-#### Java Lesson 8 :coffee:
-
-- Utilizing `classes` and `objects` to create a monster game :alien:
+#### Java Lesson 9 :coffee:
+- `Array` manipulation
+- `Array` creation and initialization
+- Utilizing the `java.util.Arrays` library functions
+  - `Arrays.copyOf()`
+  - `Arrays.copyOfRange()`
+  - `Arrays.toString()`
+  - `Arrays.sort()`
+  - `Arrays.binarySearch()`
+- Looping through arrays using `for each` loop
--- learning-java/src/LearningJava.java
+++ learning-java/src/LearningJava.java
@@ -1,8 +1,106 @@
+import java.util.Arrays;
+
 public class LearningJava {
+	
 	public static void main(String[] args) {
-		Monster Frank = new Monster();
-		Frank.name = "Frank";
-		System.out.println(Frank.name + " has an attack of " + Frank.getAttack());
+		
+		int[] randomArray;
+		
+		int[] numberArray = new int[10];
+		
+		randomArray = new int[20];
+		
+		randomArray[1] = 2;
+		
+		String[] stringArray = {"Ugh","this","is"};
+		
+		for(int i = 0; i < numberArray.length ; i++) {
+			
+			numberArray[i] = i;
+			
+		}
+		
+		int k = 1;
+		while(k <= 61) {
+			System.out.print('-');
+			k++;	
+		}
+		System.out.println();
+		
+//		for(int j = 0; j < numberArray.length; j++) {
+//			System.out.print("| " + j + " ");
+//		}
+//		System.out.println("| ");
+		
+		String[][] multiArray = new String[10][10];
+		
+		for(int i = 0; i < multiArray.length; i++) {
+			for(int j = 0; j <multiArray[i].length;j++) {
+				
+				multiArray[i][j] = i + " " + j;
+				System.out.print("| " + multiArray[i][j] + " ");
+				
+			}
+			System.out.println("| ");
+
+		}
+		
+		k = 1;
+		while(k <= 61) {
+			System.out.print('-');
+			k++;	
+		}
+		System.out.println();
+		
+		
+//		for(int row : numberArray) {
+//			System.out.print(row);
+//		}
+		
+		
+		for(String[] row: multiArray) {
+			for(String column: row) {
+				System.out.print("| " + column + " ");
+			}
+			System.out.println("| ");
+		}
+		
+		int[] numberCopy = Arrays.copyOf(numberArray, numberArray.length);
+		
+		int[] rangeCopy = Arrays.copyOfRange(numberArray, 3, 6);
+		
+		System.out.println(Arrays.toString(rangeCopy));
+		
+		for(int row: numberCopy) {
+			System.out.print(row);
+			System.out.print(" |");
+		}
+		
+		System.out.println();
+		
+		int[] moreNumbers = new int[100];
+		Arrays.fill(moreNumbers,  2);
+		
+		char[][] boardGame = new char[10][10];
+		
+		for(char[] rows: boardGame) {
+			Arrays.fill(rows,'*');
+		}
+		
+		int[] numToSort = new int[10];
+		
+		for(int i=0;i<10;i++) {
+			numToSort[i] = (int) (Math.random() * 100);
+		}
+		
+		Arrays.sort(numToSort);
+		
+		System.out.println(Arrays.toString(numToSort));
+		
+		int whereIs50 = Arrays.binarySearch(numToSort, 50);
+		
+		System.out.println(whereIs50);
 		
 	}
+	
 }
